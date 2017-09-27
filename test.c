a47757bf92f3ba32cab219fe0e929b354831a1bc
chosun <abc@chosun.ac.kr>
2017-09-27 10:08:39 +0900

    locally modified test.c

--- test.c
+++ test.c
@@ -3,5 +3,11 @@ int main()
 {
 	printf("Hello Virtual Box!!\n");
 	printf("Hello Hotfix!!\n");
+	printf("Hello Hotfix22222!!\n");
+	printf("Hello Hotfix33333!!\n");
+	printf("Hello Hotfix44444!!\n");
+	printf("Hello Hotfix55555!!\n");
+	printf("Hello Hotfix666666!!\n");
+	printf("Hello Hotfix777777777777777777777777777777777777777777777777!!\n");
 	return 0;
 }
